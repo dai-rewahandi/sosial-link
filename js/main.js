b9cf6fd89b6503e21562a3adc00c17e36b67a0b7
dai-rewahandi <niktodaire@gmail.com>
2025-01-03 16:20:35 +0700

    mengubah tautan

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
-const github = "https://github.com/nikto-de"
-const facebook = "https://www.facebook.com/profile.php?id=61567765706313"
-const twitter = "https://x.com/nikto_de"
-const instagram = "https://instagram.com/nikto__de"
-const pinterest = "https://pinterest.com/nikto_de"
-const sosial_link = [{ 0: github }, { 1: facebook }, { 2: twitter }, { 3: instagram }, { 4: pinterest }]
-const icon = document.querySelectorAll(".icon")
-icon.forEach(icon => {
+const github = "https://github.com/dai-rewahandi";
+const facebook = "https://www.facebook.com/dai.rewahandi";
+const twitter = "https://x.com/dai_rewahandi";
+const instagram = "https://instagram.com/dai.rewahandi";
+const pinterest = "https://pinterest.com/dai_rewahandi";
+const sosial_link = [
+  { 0: github },
+  { 1: facebook },
+  { 2: twitter },
+  { 3: instagram },
+  { 4: pinterest },
+];
+const icon = document.querySelectorAll(".icon");
+icon.forEach((icon) => {
   icon.addEventListener("click", () => {
-    link = `${icon.id}`
-    window.location.href = sosial_link[icon.id][icon.id]
-  })
-})
+    link = `${icon.id}`;
+    window.location.href = sosial_link[icon.id][icon.id];
+  });
+});
